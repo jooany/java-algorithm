01759ebd2124b9d2668b4571cb5d313498db089d
jooany <83267254+jooany@users.noreply.github.com>
2025-03-05 01:49:18 +0900

    [level 2] Title: 올바른 괄호, Time: 18.16 ms, Memory: 55.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -4,17 +4,16 @@ class Solution {
     boolean solution(String s) {
         Deque<Character> stack = new ArrayDeque<>();
         
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
+        char[] arr = s.toCharArray();
+        for (char c : arr) {
+            if (c == '(') {
                 stack.push('(');
                 continue;
-            }
+            } 
             
-            if (stack.size() == 0) {
+            if (stack.isEmpty() || stack.pop() == c) {
                 return false;
             }
-            
-            stack.pop();
         }
         
         return stack.isEmpty();
