8e96da676d141f5c852c6f3f78d4c0aacd4dc63b
jooany <83267254+jooany@users.noreply.github.com>
2025-03-05 17:32:11 +0900

    [level 2] Title: 주식가격, Time: 33.18 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,10 +2,12 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
+        
         int[] answer = new int[prices.length];
         
         Deque<Integer> stack = new ArrayDeque<>();
         stack.push(0);
+        
         for (int i = 1; i < prices.length; i++) {
             while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
                 answer[stack.peek()] = i - stack.pop();
