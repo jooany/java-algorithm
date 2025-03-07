1de34c07e3ee2c3a0216e674e7c593aa4c83bd2d
jooany <83267254+jooany@users.noreply.github.com>
2025-03-07 16:49:22 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 91.29 ms, Memory: 96.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,17 +2,25 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> map = new HashMap<>();
         
-        Arrays.sort(participant);
-        Arrays.sort(completion);
-        
-        for (int i = 0 ; i < completion.length; i++) {
-            
-            if (!participant[i].equals(completion[i])) {
-                return participant[i];
-            }
+        for (String p : participant) {
+            map.put(p, map.getOrDefault(p, 0) + 1);
         }
         
-        return participant[participant.length - 1];
+        for (String c : completion) {
+            if (map.get(c) == 1) {
+                map.remove(c);
+                continue;
+            }
+            
+            map.put(c, map.get(c) - 1);
+        }
+        
+        return map.entrySet()
+            .stream()
+            .map(Map.Entry::getKey)
+            .findFirst()
+            .get();
     }
 }
