0e6d537be355192e1943bec94c63e310822b1eaf
jooany <83267254+jooany@users.noreply.github.com>
2025-04-10 13:26:39 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 7.52 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,36 +1,16 @@
 import java.util.*;
 
 class Solution {
-    String ss;
     int convertCnt, removedZeroCnt;
     
     public int[] solution(String s) {
-        ss = s;
         
-        while (!"1".equals(ss)) {
-            if (ss.contains("0")) {
-                int originalLength = ss.length();
-                ss = ss.replace("0", "");
-                removedZeroCnt += originalLength - ss.length();
-            }
+        while (!"1".equals(s)) {
+            int zeroCnt = s.replace("1", "").length();  
+            removedZeroCnt += zeroCnt;
             
-            int length = ss.length();
-            Deque<Integer> stack = new ArrayDeque<>();
+            s = Integer.toBinaryString(s.length() - zeroCnt);
             
-            while (length > 1) {
-                stack.push(length % 2);
-                length /= 2;
-            }
-            
-            stack.push(length);
-            
-            StringBuilder sb = new StringBuilder();
-            
-            while (!stack.isEmpty()) {
-                sb.append(stack.pop());
-            }
-            
-            ss = sb.toString();
             convertCnt++;
         }
         
