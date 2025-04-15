88049a6449783403cd43e8a0b3f9e12189fe31cb
jooany <83267254+jooany@users.noreply.github.com>
2025-04-15 19:11:29 +0900

    [level 2] Title: 모음 사전, Time: 5.14 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,35 +1,32 @@
 class Solution {
-    static String[] alphabets;
-    static String inputWord;
-    static int answer;
-    static int cnt;
-    static StringBuilder sb = new StringBuilder();
+    String[] alphabets;
+    String inputWord;
+    int answer, cnt = 0;
     
     public int solution(String word) {
         alphabets = new String[]{"A", "E", "I", "O", "U"};
         inputWord = word;
-        cnt = -1;
         
-        search();
+        search(new StringBuilder());
         
         return answer;
     }
     
-    private static void search() {
-        cnt++;
-        
-        if (sb.length() != 0 && sb.toString().equals(inputWord)) {
-            answer = cnt;
-            return;
+    private void search(StringBuilder sb) {
+        if (sb.length() > 0) {
+            cnt++;
+            
+            if (sb.toString().equals(inputWord)) {
+                answer = cnt;
+                return;
+            }
         }
         
-        if (sb.length() == 5) {
-            return;
-        }
+        if (sb.length() == 5) return;
         
         for (int i = 0; i < 5; i++) {
             sb.append(alphabets[i]);
-            search();
+            search(sb);
             sb.deleteCharAt(sb.length() - 1);
         }
     }
