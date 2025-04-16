328ede0cbf5e296a49c9613993115f1db2d0c3f7
jooany <83267254+jooany@users.noreply.github.com>
2025-04-16 13:19:28 +0900

    [level 2] Title: 택배상자, Time: 135.84 ms, Memory: 136 MB -BaekjoonHub

---
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] order) {
+        Deque<Integer> mainContainer = new ArrayDeque<>(); 
+        for (int n = 1; n <= order.length; n++) {
+            mainContainer.offer(n);
+        }
+        
+        Deque<Integer> subContainer = new ArrayDeque<>();
+        int cnt = 0;
+        
+        for (int i = 0; i < order.length; i++) {
+            if (mainContainer.isEmpty() && subContainer.isEmpty()) {
+                break;
+            }
+            
+            int target = order[i];
+            
+            if ((mainContainer.isEmpty() || mainContainer.peek() > target)) {
+                if (target != subContainer.pop()) break;
+                
+                cnt++;
+            } 
+            
+            while (!mainContainer.isEmpty() && mainContainer.peek() <= target) {
+                if (target == mainContainer.peek()) {
+                    mainContainer.poll();
+                    cnt++;
+                    break;
+                }
+
+                subContainer.push(mainContainer.poll());
+            }
+        }
+        
+        return cnt;
+    }
+}
