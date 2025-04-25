872b526b1acb8284e336b9e8ec917e4cad9170a1
jooany <83267254+jooany@users.noreply.github.com>
2025-04-25 14:31:12 +0900

    [level 2] Title: 소수 찾기, Time: 20.65 ms, Memory: 97.8 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -40,9 +40,9 @@ class Solution {
     private boolean isPrimeNumber(int num) {
         if (num < 2) return false;
         
-        if (num > 2 && num % 2 == 0) return false;
+        if (num != 2 && num % 2 == 0) return false;
         
-        for (int i = 3; i <= Math.sqrt(num); i++) {
+        for (int i = 3; i <= Math.sqrt(num); i += 2) {
             if (num % i == 0) return false; 
         }
         
