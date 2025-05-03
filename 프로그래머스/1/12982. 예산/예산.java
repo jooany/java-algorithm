e64728ad61f041acccf28d1c46b0217471f5835b
jooany <83267254+jooany@users.noreply.github.com>
2025-05-03 16:09:40 +0900

    [level 1] Title: 예산, Time: 0.50 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        Arrays.sort(d);
+        
+        int answer = 0;
+        
+        for (int cost : d) {
+            if (budget < cost || budget == 0) {
+                break;
+            }
+            
+            budget -= cost;
+            answer++;
+        }
+        
+        return answer;
+    }
+}
