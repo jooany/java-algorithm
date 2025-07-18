74b995fe104e65f99ef7875e53a6ce7ec00e76f2
jooany <83267254+jooany@users.noreply.github.com>
2025-07-18 11:21:57 +0900

    [level 2] Title: 더 맵게, Time: 1859.34 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -2,19 +2,32 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] scoville, int K) {
+        int answer = 0;
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        int cnt = 0;
         
-        for (int sv : scoville) {
-            pq.add(sv);
+        for (int scov : scoville) {
+            pq.offer(scov);
         }
         
-        while(pq.size() >= 2 && pq.peek() < K) {
-            pq.add(pq.poll() + pq.poll() * 2);
+        if (pq.peek() >= K) {
+            return 0;
+        }
+        
+        // 만약 K보다 작아지지 않는다면, 무제한으로 돌게 된다.
+        while (!pq.isEmpty() && pq.peek() < K && pq.size() > 1) {
+            int min1 = pq.poll();
+            int min2 = pq.poll();
             
-            cnt++;
+            pq.offer(mix(min1 , min2));
+            
+            answer++;
         }
-
-        return pq.peek() >= K ? cnt : -1;
+        
+        return pq.peek() < K ? -1 : answer;
     }
+    
+    private int mix (int min1, int min2) {
+        return min1 + min2 * 2;
+    }
+    
 }
