db5f514d766f0b4cc7878968d1891dd79584d8e8
jooany <83267254+jooany@users.noreply.github.com>
2025-07-18 10:24:22 +0900

    [level 2] Title: 전화번호 목록, Time: 345.65 ms, Memory: 98 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,17 +2,19 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
+        
+        // 접두사가 포함된다면 문자열을 사전순 정렬했을 때, 양 옆으로 정렬될 수 밖에 없다.
+        
         Arrays.sort(phone_book);
         
         for (int i = 0; i < phone_book.length - 1; i++) {
-            String a = phone_book[i];
-            String b = phone_book[i + 1];
             
-            if (b.length() < a.length()) {
-                continue;
-            }
+            String curr = phone_book[i];
+            String next = phone_book[i + 1];
             
-            if (b.substring(0, a.length()).equals(a)) {
+            if (curr.length() < next.length() 
+                && curr.equals(next.substring(0, curr.length()))
+               ) {
                 return false;
             }
         }
