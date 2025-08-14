381e183987f8a56fda46c50807ec69a2e858bd8a
jooany <83267254+jooany@users.noreply.github.com>
2025-08-14 21:53:45 +0900

    [level 2] Title: [1차] 캐시, Time: 51.21 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 class Solution {
     public int solution(int cacheSize, String[] cities) {
+        
         int answer = 0;
         Deque<String> queue = new ArrayDeque<>();
         
