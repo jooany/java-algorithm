7cb37f17b15416718d0d3fea6b154eaf720661d5
jooany <83267254+jooany@users.noreply.github.com>
2025-08-20 15:03:48 +0900

    [Silver III] Title: N과 M (1), Time: 660 ms, Memory: 54396 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,45 +1,42 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        
-        boolean[] visited = new boolean[N + 1];
-     
-        process(N, M, new StringBuilder(""), visited);
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        boolean[] visited = new boolean[n + 1];
+        StringBuilder sb = new StringBuilder();
+
+        recursive(sb, 0, n, m, visited);
     }
     
-    // N 만큼의 for 문으로 재귀 호출한다.
-    public static void process(int N, int M, StringBuilder sb, boolean[] visited) {
+    public static void recursive(StringBuilder sb, int cnt, int n, int m, boolean[] visited) {
         
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i <= n; i++) {
             if (visited[i]) continue;
             
             visited[i] = true;
+            int length = sb.length();
             
-            int len = sb.length();
-            if (len == 0) {
+            if (length == 0) {
                 sb.append(i);
             } else {
-                sb.append(" " + i);
+               sb.append(" " + i);
             }
             
-            // 총 M개일 때 출력, 아니면 다음으로
-            if (sb.length() == 2 * M - 1) {
+            if (cnt + 1 == m) {
                 System.out.println(sb);
             } else {
-                process(N, M, sb, visited);
+                recursive(sb, cnt + 1, n, m, visited);
             }
             
             visited[i] = false;
-            
-            sb.setLength(len);
+            sb.setLength(length);
         }
+        
     }
-    
 }
