12936991491ad4aa1ffef3a218e278d9488f1557
jooany <83267254+jooany@users.noreply.github.com>
2025-10-15 16:18:02 +0900

    [level 3] Title: 네트워크, Time: 0.26 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,32 +1,28 @@
-import java.util.*;
-
 class Solution {
-    int cpuCnt;
-    boolean[] visited;
-    int[][] cpus;
+    int answer = 0;
     
     public int solution(int n, int[][] computers) {
-        int answer = 0;
-        cpuCnt = n;
-        cpus = computers;
-        visited = new boolean[n];
+        boolean[] visited = new boolean[n];
         
         for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                dfs(i);
-                answer++;
-            }
+            // i 노드를 방문하지 않았다면, 네트워크 + 1 하고 방문 표시한다.
+            // i 노드부터 computers & DFS를 활용하여 연결된 것을 모두 탐색하고 방문 표시한다. (양방향 주의)
+            
+            if (visited[i]) continue;
+
+            answer++;
+            dfs(i, visited, computers);
         }
         
         return answer;
     }
     
-    private void dfs(int index) {
-        visited[index] = true;
+    public void dfs(int i, boolean[] visited, int[][] computers) {
+        visited[i] = true;
         
-        for (int i = 0; i < cpuCnt; i++) {
-            if (!visited[i] && cpus[index][i] == 1) {
-                dfs(i);
+        for (int j = 0; j < computers.length; j++) {
+            if (!visited[j] && computers[i][j] == 1) {
+                dfs(j, visited, computers);
             }
         }
     }
