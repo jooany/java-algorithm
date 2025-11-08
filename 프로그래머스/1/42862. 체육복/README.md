5c8506696fee2911ded27f2b9c57a3752e0b823e
jooany <83267254+jooany@users.noreply.github.com>
2025-11-08 14:16:02 +0900

    [level 1] Title: 체육복, Time: 1.53 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
 
 ### 성능 요약
 
-메모리: 86.5 MB, 시간: 0.05 ms
+메모리: 84.9 MB, 시간: 1.53 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 06일 14:16:12
+2025년 11월 08일 14:15:05
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,37 +2,36 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0;
-        
-        int[] clothes = new int[n + 1];
-        Arrays.fill(clothes, 1);
-        
-        for (int l : lost) {
-            clothes[l]--;
-        }
+        HashSet<Integer> reserves = new HashSet<>();
+        LinkedHashSet<Integer> losts = new LinkedHashSet<>();
         
         for (int r : reserve) {
-            clothes[r]++;
+            reserves.add(r);
         }
         
-        for (int i = 1; i <= n; i++) {
+        Arrays.sort(lost);
+        
+        for (int l : lost) {
+            if (reserves.contains(l)) {
+                reserves.remove(l);
+                continue;
+            }
             
-            if (clothes[i] > 0) {
-                
-                answer++;
-                
-            } else if (clothes[i] == 0) {
-
-                if (i > 1 && clothes[i - 1] == 2) {
-                    answer++;
-                } else if (i + 1 <= n && clothes[i + 1] == 2) {
-                    answer++;
-                    clothes[i + 1]--;
-                }
-                
+            losts.add(l);
+        }
+        
+        int lostTotal = 0;
+        
+        for (int cur : losts) {
+            if (reserves.contains(cur - 1)) {
+                reserves.remove(cur - 1);
+            } else if (reserves.contains(cur + 1)) {
+                reserves.remove(cur + 1);
+            } else {
+                lostTotal++;
             }
         }
         
-        return answer;
+        return n - lostTotal;
     }
 }
