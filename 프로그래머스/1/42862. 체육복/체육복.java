f9cebaf394468393ab0f1c53bbb80dab3ef4f02c
jooany <83267254+jooany@users.noreply.github.com>
2025-11-08 14:27:27 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,33 +2,26 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        HashSet<Integer> reserves = new HashSet<>();
-        LinkedHashSet<Integer> losts = new LinkedHashSet<>();
-        
-        for (int r : reserve) {
-            reserves.add(r);
-        }
-        
-        Arrays.sort(lost);
+        int[] clothes = new int[n + 2];
         
         for (int l : lost) {
-            if (reserves.contains(l)) {
-                reserves.remove(l);
-                continue;
-            }
-            
-            losts.add(l);
+            clothes[l]--;
+        }
+        
+        for (int r : reserve) {
+            clothes[r]++;
         }
         
         int lostTotal = 0;
-        
-        for (int cur : losts) {
-            if (reserves.contains(cur - 1)) {
-                reserves.remove(cur - 1);
-            } else if (reserves.contains(cur + 1)) {
-                reserves.remove(cur + 1);
-            } else {
-                lostTotal++;
+        for (int i = 1; i <= n; i++) {
+            if (clothes[i] == -1) {
+                if (clothes[i - 1] == 1) {
+                    clothes[i - 1]--;
+                } else if (clothes[i + 1] == 1) {
+                    clothes[i + 1]--;
+                } else {
+                    lostTotal++;
+                }
             }
         }
         
