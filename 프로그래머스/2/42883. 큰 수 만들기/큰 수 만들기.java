332e76fd43039010669424fe12bebb0b99150c8c
jooany <83267254+jooany@users.noreply.github.com>
2025-11-09 14:50:46 +0900

    [level 2] Title: 큰 수 만들기, Time: 28.98 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -3,19 +3,20 @@ import java.util.*;
 class Solution {
     public String solution(String number, int k) {
         StringBuilder sb = new StringBuilder();
+        int remain = k;
         
         for (char c : number.toCharArray()) {
-            while (k > 0 && sb.length() > 0 && sb.charAt(sb.length() - 1) < c) {
+            
+            while (remain > 0 && sb.length() > 0 && sb.charAt(sb.length() - 1) < c) {
                 sb.deleteCharAt(sb.length() - 1);
-                k--;
+                remain--;
             }
             
             sb.append(c);
         }
         
-        while (k > 0) {
-            sb.deleteCharAt(sb.length() - 1);
-            k--;
+        if (remain > 0) {
+            sb.setLength(sb.length() - remain);
         }
         
         return sb.toString();
