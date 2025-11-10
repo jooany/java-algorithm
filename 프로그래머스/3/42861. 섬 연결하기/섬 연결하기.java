01418d99e724732eb1d6ca587cc1ad6909a7843d
jooany <83267254+jooany@users.noreply.github.com>
2025-11-10 19:24:06 +0900

    [level 3] Title: 섬 연결하기, Time: 0.62 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 class Solution {
-    private static int[] parents;
+    int[] parents;
     
     public int solution(int n, int[][] costs) {
         parents = new int[n];
@@ -13,24 +13,23 @@ class Solution {
         Arrays.sort(costs, (o1, o2) -> o1[2] - o2[2]);
         
         int answer = 0;
-        for (int[] cost : costs) {
-            int root1 = find(cost[0]);
-            int root2 = find(cost[1]);
-        
+        for (int[] edge : costs) {
+            int root1 = find(edge[0]);
+            int root2 = find(edge[1]);
+            
             if (root1 != root2) {
                 parents[root2] = root1;
-                answer += cost[2];
+                
+                answer += edge[2];
             }
         }
         
         return answer;
     }
     
-    private static int find(int island) {
-        if (parents[island] == island) {
-            return island;
-        }
+    private int find(int a) {
+        if (parents[a] == a) return a;
         
-        return find(parents[island]);
+        return find(parents[a]);
     }
 }
