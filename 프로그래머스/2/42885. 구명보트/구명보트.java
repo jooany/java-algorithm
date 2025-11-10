7d283ffceafbe6ea74d099bd93466346860ac590
jooany <83267254+jooany@users.noreply.github.com>
2025-11-10 12:21:16 +0900

    [level 2] Title: 구명보트, Time: 11.66 ms, Memory: 57.8 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -2,18 +2,27 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] people, int limit) {
-        int i = 0;
-        int j = people.length - 1;
+        /*
+            최대한 무게가 많이 나가는 사람과 적게 나가는 사람을 짝 지어야 한다.
+        */
         int answer = 0;
-        
+        int len = people.length;
         Arrays.sort(people);
         
-        while (i <= j) {
-            if (people[i] + people[j] <= limit) {
-                i++;
+        int p1 = 0;
+        int p2 = len - 1;
+        
+        while (p1 < p2) {
+            if (people[p1] + people[p2] <= limit) {
+                p1++;
+                p2--;
+            } else {
+                p2--;
             }
-            
-            j--;
+            answer++;
+        }
+        
+        if (p1 == p2) {
             answer++;
         }
         
