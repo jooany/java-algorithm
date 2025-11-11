db02b0878c5c5b75aba9fe2f5ef4b84890cfdfdd
jooany <83267254+jooany@users.noreply.github.com>
2025-11-11 11:39:14 +0900

    [level 2] Title: 타겟 넘버, Time: 7.78 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,27 +1,32 @@
 class Solution {
-    static int[] nums;
+    static int n;
     static int targetNum;
-    int answer = 0;
+    static boolean[] visited;
+    static int[] nums;
+    int answer;
     
-    // DFS 풀이
     public int solution(int[] numbers, int target) {
+        /*
+        타겟 넘버가 되는 모든 경우의 수 -> 완전 탐색
+        */
+        
         nums = numbers;
+        n = numbers.length;
         targetNum = target;
+        visited = new boolean[n];
         
         dfs(0, 0);
         
         return answer;
     }
     
-    private void dfs(int i, int result) {
-        if (i == nums.length) {
+    private void dfs(int index, int result) {
+        if (index == n) {
             if (result == targetNum) answer++;
-            
             return;
         }
         
-        dfs(i + 1, result + nums[i]);
-        dfs(i + 1, result - nums[i]);
+        dfs(index + 1, result + nums[index]);
+        dfs(index + 1, result - nums[index]);
     }
-    
 }
