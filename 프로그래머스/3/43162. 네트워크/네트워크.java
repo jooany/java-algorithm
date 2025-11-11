a9870ad441dca4b6ffa8fd8de65dfa034e11112f
jooany <83267254+jooany@users.noreply.github.com>
2025-11-11 18:34:01 +0900

    [level 3] Title: 네트워크, Time: 0.55 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -4,37 +4,26 @@ class Solution {
     int answer = 0;
     
     public int solution(int n, int[][] computers) {
-        List<Integer>[] graph = new ArrayList[n];
-        for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i != j && computers[i][j] == 1) {
-                    graph[i].add(j);
-                }
-            }
-        }
-        
         boolean[] visited = new boolean[n];
         
         for (int i = 0; i < n; i++) {
-            dfs(i, graph, visited);
+            dfs(i, computers, visited);
         }
         
         return answer;
     }
     
-    private void dfs(int curr, List<Integer>[] graph, boolean[] visited) {
+    private void dfs(int curr, int[][] computers, boolean[] visited) {
         if (!visited[curr]) answer++;
         
         visited[curr] = true;
         
-        for (Integer next : graph[curr]) {
-            if (visited[next]) continue;
+        for (int next = 0; next < computers.length; next++) {
+            if (next == curr || computers[curr][next] != 1 || visited[next]) continue;
             
             visited[next] = true;
 
-            dfs(next, graph, visited);
+            dfs(next, computers, visited);
         }
     }
 }
