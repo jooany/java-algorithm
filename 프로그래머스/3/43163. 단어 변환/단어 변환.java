04865f4116b2c4927e94e33c128bda8fc8ec2de9
jooany <83267254+jooany@users.noreply.github.com>
2025-11-14 13:09:01 +0900

    [level 3] Title: 단어 변환, Time: 0.69 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -3,12 +3,6 @@ import java.util.*;
 class Solution {
     public int solution(String begin, String target, String[] words) {
         int answer = 0;
-
-        // 종료 : words에 target이 없으면 return 0
-        
-        // begin 알파벳과 한개의 알파벳만 다른 알파벳을 찾아서 큐에 넣는다.
-        // 큐 추출, 한개의 알파벳만 다른 알파벳을 찾아서 큐에 넣는다.
-        // O(N^2) = 3 * 50 * 50
         
         HashSet<String> wordSet = new HashSet<>();
         for (String w : words) {
@@ -18,6 +12,9 @@ class Solution {
         if (!wordSet.contains(target)) {
             return 0;
         }
+        
+        HashSet<String> visited = new HashSet<>();
+        visited.add(begin);
         
         Deque<Word> q = new ArrayDeque<>();
         q.offer(new Word(begin, 0));
@@ -30,6 +27,8 @@ class Solution {
             }
             
             for (String word : wordSet) {
+                if (visited.contains(word)) continue;
+                
                 int diff = 0;
                 
                 for (int i = 0; i < begin.length(); i++) {
@@ -38,7 +37,9 @@ class Solution {
                 
                 if (diff != 1) continue;
                 
+                visited.add(word);
                 q.offer(new Word(word, curr.cnt + 1));
+                
             }
         }
         
